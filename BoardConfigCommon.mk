4c76232ddd803cf495c41ba1e881f724f461e419
johnmart19 <johnivan19999@gmail.com>
2022-01-17 18:49:04 +0200

    sm8250-common: Update security patch according to used CAF tag Tag: LA.UM.9.12.r1-13300-SMxx50.QSSI12.0 Security Patch: 2021-09-05

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -217,7 +217,7 @@ SOONG_CONFIG_XIAOMI_KONA_ROOTDIR_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-08-01
+VENDOR_SECURITY_PATCH := 2021-09-05
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
